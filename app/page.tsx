3655243495656a90c8b866c94060bcf3694e772a
Tomas <tmercadoslp@gmail.com>
2023-08-23 16:01:34 -0300

    feat: add intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,56 @@
+import api from "@/api";
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function NotSelectedPage() {
-  return <>Nothing here for now...</>;
+  const coins = await api.coins.get();
+  const coinsToOmit = ["usd-coin", "tether"];
+  const coinsToDisplay = coins
+    .filter((coin) => !coinsToOmit.includes(coin.id))
+    .slice(0, 6);
+
+  return (
+    <div className="flex flex-col gap-6 text-center justify-center h-full">
+      <h2 className="text-4xl">
+        Welcome to{" "}
+        <span className="underline -underline-offset-1 decoration-4 decoration-primary">
+          Crypto Things
+        </span>{" "}
+        👋🏻 💸
+      </h2>
+
+      <p>
+        This is the place to get sad that you {`didn't`} make that crypto
+        investment you believed in so much 😪, or relieved that you {`didn't`}{" "}
+        😅
+      </p>
+
+      <p>
+        Click on any of the cryptocurrencies on the left to start your
+        calculations 🧮
+      </p>
+
+      <p>Or click on one of these to get started:</p>
+
+      <div className="grid grid-cols-6 place-items-center">
+        {coinsToDisplay.map((coin) => (
+          <div
+            className="tooltip tooltip-bottom tooltip-primary"
+            data-tip={coin.name}
+            key={coin.id}
+          >
+            <Link href={`${coin.id}`}>
+              <Image
+                src={coin.image}
+                alt={coin.name}
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title, theme, themes }) => {
   return (
     <header className="flex justify-between p-4 h-[68px] bg-primary text-primary-content">
-      <h1 className="text-3xl font-bold">{title}</h1>
+      <h1 className="text-3xl font-bold">💸🕯️{title}</h1>
 
       <ToggleThemeButton theme={theme} list={themes} />
     </header>
